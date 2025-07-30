0af0550bafe2d82522bffdf1baa699720a6fba5d
l <47887093+lmjpg@users.noreply.github.com>
2025-07-30 17:59:55 +1000

    Switch to svg board, add basic piece placing, and add README

--- tinychess.go
+++ tinychess.go
@@ -1,35 +1,28 @@
 package main
 
 import (
-	"image/color"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 func main() {
 	tinyChess := app.New()
 	window := tinyChess.NewWindow("TinyChess")
 
-	var light_square_color = color.RGBA{150, 74, 34, 255}
-	var dark_square_color = color.RGBA{238, 220, 151, 255}
+	image := canvas.NewImageFromFile("images/chessboard.svg")
+	image.FillMode = canvas.ImageFillOriginal
 
 	var squares []fyne.CanvasObject
-	for i := range 64 {
-		var square_color color.Color
-		if i%2 == (i/8)%2 {
-			square_color = light_square_color
-		} else {
-			square_color = dark_square_color
-		}
-		squares = append(squares, canvas.NewRectangle(square_color))
+	for range 64 {
+		squares = append(squares, canvas.NewImageFromFile("images/pawn_black.svg"))
 	}
 
 	grid := container.NewGridWithRows(8, squares...)
 
-	window.SetContent(grid)
+	window.SetContent(container.New(layout.NewStackLayout(), image, grid))
 	window.Resize(fyne.NewSize(600, 600))
 	window.ShowAndRun()
 }
