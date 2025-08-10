b37173cdec766a29da6cc3f28b6a10da7c15a250
l <47887093+lmjpg@users.noreply.github.com>
2025-08-11 00:33:30 +1000

    Add promotion

--- tinychess.go
+++ tinychess.go
@@ -16,11 +16,14 @@ import (
 var content embed.FS
 
 type TinyChess struct {
-	Game        *Game
-	Squares     []*PieceWidget
-	Overlay     []*widget.Icon
-	Resources   Resources
-	SelectedPos *Position
+	Game             *Game
+	Window           *fyne.Window
+	Squares          []*PieceWidget
+	Overlay          []*widget.Icon
+	PromotionOverlay *fyne.Container
+	Resources        Resources
+	SelectedPos      *Position
+	PromotionMove    *Move
 }
 
 type Resources struct {
@@ -34,6 +37,12 @@ type PieceWidget struct {
 	Pos     Position
 }
 
+type PromotionWidget struct {
+	widget.Icon
+	Session *TinyChess
+	Piece   int
+}
+
 func newPieceWidget(session *TinyChess, x int, y int, res fyne.Resource) *PieceWidget {
 	widget := &PieceWidget{Session: session, Pos: Position{x, y}}
 	widget.ExtendBaseWidget(widget)
@@ -43,6 +52,10 @@ func newPieceWidget(session *TinyChess, x int, y int, res fyne.Resource) *PieceW
 }
 
 func (t *PieceWidget) Tapped(_ *fyne.PointEvent) {
+	if t.Session.PromotionMove != nil {
+		return
+	}
+
 	clickedPos := t.Pos
 	if t.Session.Game.Turn == Black {
 		clickedPos.Y = 7 - clickedPos.Y
@@ -58,15 +71,52 @@ func (t *PieceWidget) Tapped(_ *fyne.PointEvent) {
 		}
 
 	} else if t.Session.SelectedPos != nil {
-		moveSuccessful := movePiece(t.Session.Game, *t.Session.SelectedPos, clickedPos, nil, true)
-		t.Session.SelectedPos = nil
-		_, ok := t.Session.Game.Board[clickedPos]
-		if !moveSuccessful && ok {
-			t.Session.SelectedPos = &clickedPos
+		piece, ok := t.Session.Game.Board[*t.Session.SelectedPos]
+		isPromoting := false
+		if ok && piece.Type == Pawn && (clickedPos.Y == 0 || clickedPos.Y == 7) {
+			var promotionMove *Move
+			isPromoting, promotionMove = isValidMove(t.Session.Game, *t.Session.SelectedPos, clickedPos)
+			if isPromoting {
+				t.Session.PromotionMove = promotionMove
+				t.Session.PromotionOverlay.Show()
+			}
+		}
+
+		if !isPromoting {
+			moveSuccessful := movePiece(t.Session.Game, *t.Session.SelectedPos, clickedPos, nil, true)
+			t.Session.SelectedPos = nil
+			_, ok = t.Session.Game.Board[clickedPos]
+			if !moveSuccessful && ok {
+				t.Session.SelectedPos = &clickedPos
+			}
 		}
 	}
 
 	updateSquares(t.Session)
+}
+
+func newPromotionWidget(session *TinyChess, piece int) *PromotionWidget {
+	widget := &PromotionWidget{Session: session, Piece: piece}
+	widget.ExtendBaseWidget(widget)
+	widget.SetResource(getPieceResource(Piece{Type: piece, Colour: session.Game.Turn}, session.Resources))
+	return widget
+}
+
+func (p *PromotionWidget) Tapped(e *fyne.PointEvent) {
+	ok := movePiece(p.Session.Game, *p.Session.SelectedPos, p.Session.PromotionMove.Pos, p.Session.PromotionMove, false)
+	if !ok {
+		log.Fatalf("Move from %v to %v was invalid despite being valid when checked earlier (PromotionWidget.Tapped)\n", p.Session.SelectedPos, p.Session.PromotionMove.Pos)
+	}
+	p.Session.PromotionOverlay.Hide()
+
+	piece := p.Session.Game.Board[p.Session.PromotionMove.Pos]
+	piece.Type = p.Piece
+	p.Session.Game.Board[p.Session.PromotionMove.Pos] = piece
+
+	p.Session.SelectedPos = nil
+	p.Session.PromotionMove = nil
+
+	updateSquares(p.Session)
 }
 
 func getPieceResource(piece Piece, resources Resources) fyne.Resource {
@@ -80,7 +130,7 @@ func getSquareIndexFromPosition(pos Position, turn int) int {
 	return pos.X + pos.Y*8
 }
 
-func createWindowFromBoard(tinychess *TinyChess, w fyne.Window) ([]*PieceWidget, []*widget.Icon) {
+func createWindowFromBoard(tinychess *TinyChess, w fyne.Window) ([]*PieceWidget, []*widget.Icon, *fyne.Container) {
 	var squares = make([]*PieceWidget, 64)
 	var overlay = make([]*widget.Icon, 64)
 
@@ -93,6 +143,11 @@ func createWindowFromBoard(tinychess *TinyChess, w fyne.Window) ([]*PieceWidget,
 		squares[getSquareIndexFromPosition(pos, tinychess.Game.Turn)].SetResource(getPieceResource(piece, tinychess.Resources))
 	}
 
+	var promotionOptions []fyne.CanvasObject
+	for _, piece := range []int{Queen, Rook, Bishop, Knight} {
+		promotionOptions = append(promotionOptions, newPromotionWidget(tinychess, piece))
+	}
+
 	// Why can't this be type cast instead?
 	var squares2 = make([]fyne.CanvasObject, len(squares))
 	var overlay2 = make([]fyne.CanvasObject, len(overlay))
@@ -103,12 +158,15 @@ func createWindowFromBoard(tinychess *TinyChess, w fyne.Window) ([]*PieceWidget,
 
 	grid := container.NewGridWithColumns(8, squares2...)
 	gridOverlay := container.NewGridWithColumns(8, overlay2...)
+	promotionOverlay := container.NewGridWithColumns(4, promotionOptions...)
 	chessboard := canvas.NewImageFromResource(tinychess.Resources.ChessBoard)
 	chessboard.FillMode = canvas.ImageFillOriginal
 
-	w.SetContent(container.New(layout.NewStackLayout(), chessboard, grid, gridOverlay))
+	promotionOverlay.Hide()
 
-	return squares, overlay
+	w.SetContent(container.New(layout.NewStackLayout(), chessboard, grid, gridOverlay, promotionOverlay))
+
+	return squares, overlay, promotionOverlay
 }
 
 func updateSquares(tinychess *TinyChess) {
@@ -193,9 +251,9 @@ func main() {
 		}
 	}
 
-	tinychess := TinyChess{Game: getInitialGame(), Squares: nil, Overlay: nil, Resources: resources, SelectedPos: nil}
+	tinychess := TinyChess{Game: getInitialGame(), Window: &w, Squares: nil, Overlay: nil, PromotionOverlay: nil, Resources: resources, SelectedPos: nil, PromotionMove: nil}
 
-	tinychess.Squares, tinychess.Overlay = createWindowFromBoard(&tinychess, w)
+	tinychess.Squares, tinychess.Overlay, tinychess.PromotionOverlay = createWindowFromBoard(&tinychess, w)
 
 	w.Resize(fyne.NewSize(600, 600))
 	w.ShowAndRun()
